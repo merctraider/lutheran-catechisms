3edc68515a1340d2cba9fa495ae785fd758d2f77
merctraider <47917658+merctraider@users.noreply.github.com>
2022-08-21 18:48:01 +0800

    added navigation

--- src/Components/CatechismChiefPart.js
+++ src/Components/CatechismChiefPart.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import CatechismSectionHeading from './CatechismSectionHeading'
 import CatechismSectionIndex from './CatechismSectionIndex';
+import NavigationArrows from './NavigationArrows';
 export default function CatechismChiefPart({id, translation}) {
     const heading = translation.heading; 
 
   return (
     <div id={id} className="section">
+        <NavigationArrows/>
         <CatechismSectionHeading title={heading.title} subtitle={heading.subtitle}/>
         <CatechismSectionIndex sections={translation.sections}/>
+        <NavigationArrows/>
     </div>
   )
 }
