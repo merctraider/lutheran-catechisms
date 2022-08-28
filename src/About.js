f9c9678427182bc6de923483d9a6da9cd43ef8e6
merctraider <47917658+merctraider@users.noreply.github.com>
2022-08-28 19:24:50 +0800

    license update

--- src/About.js
+++ src/About.js
@@ -15,7 +15,7 @@ export default function About() {
                 <p>The Copyright of the text on this site are subject to the copyrights of the various owners.</p>
                 <p><strong>Henkel BoC (1854)</strong>: Adapted from the <i>Henkel Electronic Edition of the Book of Concord</i>, Copyright © 2008 <a href="http://grabauski.com" target="_blank">http://grabauski.com</a>. Used by permission. All Rights Reserved. </p>
                 <p><strong>Henry E. Jacobs (1882)</strong>: Adapted from <a href="https://www.lutheranlibrary.org/583-jacobs-luthers-small-catechism/" target="_blank"><i>The Small Catechism of Martin Luther edited by Henry Eyster Jacobs</i></a>, published by <a href='https://LutheranLibrary.org' target="_blank">LutheranLibrary.org</a> under <a href='https://creativecommons.org/licenses/by/4.0/' target="_blank">CC by 4.0</a>. Minor changes such as formatting of text were made in the adaptation of this translation. You may freely copy and redistribute the text in this translation according to the terms of this license.</p>
-                <p><strong>2022 Giáo Lý Căn Bản (Vietnamese) </strong>: Vietnamese translation Copyright © Sebastian Tran Ken and Ignatius Trần Anh Khoa. All Rights Reserved. </p>
+                <p><strong>2022 Giáo Lý Căn Bản (Vietnamese) </strong>: Vietnamese translation by Sebastian Tran Ken and Ignatius Trần Anh Khoa. This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">CC 4.0</a>. You may freely copy and redistribute the text in this translation according to the terms of this license.</p>
             </div>
 
         </div>
