b79344c93f2b66ef09338b6446d48f11820530af
merctraider <47917658+merctraider@users.noreply.github.com>
2022-09-10 17:45:59 +0800

    experimentation

--- src/Components/CatechismExplanation.js
+++ src/Components/CatechismExplanation.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Collapse, Card } from 'react-bootstrap';
+import { Collapse, CloseButton } from 'react-bootstrap';
 import ExplanationIndex from './ExplanationIndex';
 import { ExplanationHandler } from '../lib/ExplanationHandler';
+import {useState} from 'react';     
 
 
-export default function CatechismExplanation({ partID, id, display }) {
+export default function CatechismExplanation({ partID, id, display}) {
     const handler = new ExplanationHandler(partID, id); 
-
+    
     console.log(handler.explanations); 
     return (
         <Collapse in={display}>
             <div>
+                
                 <div className='explanation'>
                     <div className='container my-2'>
-                    <h5>Explanations</h5>
+                    <h5> Explanations</h5>
+                    
                     
                     </div>
                     
--- src/Components/CatechismSectionHeading.js
+++ src/Components/CatechismSectionHeading.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { Interweave } from 'interweave'
 import CatechismExplanation from './CatechismExplanation'
 import { useState} from 'react'
-import {Button} from 'react-bootstrap'
+import { CloseButton } from 'react-bootstrap'
+
 
 export default function CatechismSectionHeading({title, subtitle, partID}) {
   const [showExplanation, setShowExplanation] = useState(false); 
   return (
-    <div className="container my-5 verse" onClick={(e) => {setShowExplanation(!showExplanation)}}>
-          
+    <div className="container my-5 verse">
           <h1 >{title} </h1>
           <p className="lead"><Interweave content={subtitle}/></p>
-          <CatechismExplanation partID={partID} id="heading" display={showExplanation}/>
+          <CloseButton onClick={(e) => {setShowExplanation(!showExplanation)}}/> 
+            <CatechismExplanation partID={partID} id="heading" display={showExplanation}/>
+          
           
     </div>
   )
